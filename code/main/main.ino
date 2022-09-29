96814659d8173f47ac56f1c2a52bc16bcd44dba0
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2022-09-29 16:22:40 +0530

    Hotspot setup with SSID

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,5 +1,17 @@
+#include <ESPAsyncWebServer.h> //importing the Asynchronous server header
+
+AsyncWebServer server(80); //server listening at port 80 i.e HTTP port
+
+//defining function for invalid requests
+void notFound(AsyncWebServerRequest *request){
+  request->send(404,"text/plain","Not found");
+}
+
 void setup() {
-  //hello world
+  Serial.begin(115200); //baud rate
+  WiFi.softAP("esp32",""); //hotspot with SSID and password is empty
+  Serial.println("IP: "); 
+  Serial.println(WiFi.softAPIP()); //IP of the microcontroller will be printed on serial monitor
 
 }
 
