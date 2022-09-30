190245e7e590405329c4fc68cbb5f3fc85369f90
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2022-09-30 17:58:08 +0530

    Added HTML form but not yet integrated with server

--- code/main/main.ino
+++ code/main/main.ino
@@ -1,4 +1,8 @@
 #include <ESPAsyncWebServer.h> //importing the Asynchronous server header
+#include <ESPmDNS.h>
+#include <WebSocketsServer.h>
+#include <ArduinoJson.h>
+#define LED1 13
 
 AsyncWebServer server(80); //server listening at port 80 i.e HTTP port
 
@@ -12,7 +16,7 @@ void setup() {
   WiFi.softAP("esp32",""); //hotspot with SSID and password is empty
   Serial.println("IP: "); 
   Serial.println(WiFi.softAPIP()); //IP of the microcontroller will be printed on serial monitor
-
+  server.begin(); //start the async web server
 }
 
 void loop() {
