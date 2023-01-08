89305470968a26670320c43a0c788740c4955836
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2023-01-09 00:37:08 +0530

    JSON parsing successful
    DISCLAIMER: LONG ONE
    So, module A can send a string which is basically in JSON format. However, this was not easy.
    The datatype of JSON_data is String which is available in C++. The problem however is that, this cannot be written to
    serial connection. Only char array i.e C type string is supported. Therefore I had to use .toCharArray to convert the String
    to char[] and save it to a char[] buffer. Only then I could write it to serial. Also had to add 1 to buffersize to fit the last
    curly brace '}' of JSON_data.
    
    On module B I used the ArduinoJSON.h file for JSON deserialization/parsing and extracted the data.

--- code/receiver/receiver.ino
+++ code/receiver/receiver.ino
@@ -1,5 +1,7 @@
 // #include <HardwareSerial.h>
 // HardwareSerial Serial2(2);
+#include <ArduinoJson.h>
+
 void setup() {
   // put your setup code here, to run once:
   Serial2.begin(115200, SERIAL_8N1, 16, 17);
@@ -12,9 +14,23 @@ void loop() {
   // put your main code here, to run repeatedly:
   while(Serial2.available()){
     String str = Serial2.readString();
-    delay(1000);
+    delay(2000);
     Serial.println(str);
-    Serial.print("\n");       
+    Serial.print("\n");
+    DynamicJsonDocument doc(200); 
+    DeserializationError error = deserializeJson(doc,str); 
+    if (error){ //if there is any error, print it
+      Serial.print("Deserialization failed");
+      Serial.println(error.c_str());
+      return;
+    }
+    int lightdata = doc["lightdata"];
+    int moisdata = doc["moisdata"];
+    float tempdata = doc["tempdata"];
+    Serial.println(lightdata);
+    Serial.println(moisdata);
+    Serial.println(tempdata);
+    delay(1000);
   }
 
 }
