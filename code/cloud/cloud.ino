ee82e6ba3432717b64e64e0ffe34876197994d53
Subhodeep Sarkar <subhodeep.sofficial@gmail.com>
2023-01-08 13:55:09 +0530

    Added WifiManager for ESP32
    Basically copied the example from WifiManager > OnDemand > OnDemandConfigPortal
    with slight modifications.
    Modifications:
    TRIGGER PIN -> 15 (previously 0)
    resetsettings()
    
    Whenever the push button is pressed the ESP32 will delete all the previous WiFi credentials
    and go into AP mode. We can connect any external device to access it using 192.168.4.1, although it automatically opens page on successfull
    connection. Then proceed with 'configure wifi' and it will scan for available networks and you have to select one of them and enter password
    to connect. There is a timer of 180 seconds and after that the hotspot (AP) will turn off.

--- code/cloud/cloud.ino
+++ code/cloud/cloud.ino
@@ -1,9 +1,38 @@
+#include <WiFiManager.h>
+#define TRIGGER_PIN 15
+int timeout = 180;
 void setup() {
-  // put your setup code here, to run once:
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP  
+  Serial.begin(115200);
+  Serial.println("\n Starting");
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
 
 }
 
 void loop() {
+    // is configuration portal requested?
+  if ( digitalRead(TRIGGER_PIN) == LOW) {
+    WiFiManager wm;    
+
+    //reset settings - for testing
+    wm.resetSettings();
+  
+    // set configportal timeout
+    wm.setConfigPortalTimeout(timeout);
+
+    if (!wm.startConfigPortal("OnDemandAP")) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.restart();
+      delay(5000);
+    }
+
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+
+  }
+
   // put your main code here, to run repeatedly:
 
 }
